68e2e8610a95516d0fa2130cf37fbf2794fc249e
BossBoxing <boss_slam@hotmail.com>
2022-04-01 15:16:55 +0700

    Fix index.html less input column and it can predict

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     output = round(prediction[0], 2) 
-    return render_template('index.html', prediction_text='CO2    Emission of the vehicle is :{}'.format(output))
+    return render_template('index.html', prediction_text='Your Result :{}'.format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
