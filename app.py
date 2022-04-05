891db3ac391a3ebdd3640b3c398dddeb3f17f066
BillyForce43 <nattawat_ch@kkumail.com>
2022-04-05 19:52:39 +0700

    edit index and result

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #import libraries
 import numpy as np
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,flash
 import pickle#Initialize the flask App
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mykey'
 model = pickle.load(open('model.pkl', 'rb'))
 
 #default page of our web-app
